8f927af952acb80f430b8a5efc328b6edf8456bb
bastianolea <bastianolea@gmail.com>
2025-04-03 20:11:45 -0300

    Initial commit

---
+++ configurar_github.R
@@ -0,0 +1,19 @@
+# https://bastianolea.rbind.io/blog/r_introduccion/tutorial_github/
+# https://bastianolea.rbind.io/blog/tutorial_quarto_github_pages/
+
+install.packages("usethis")
+
+library(usethis)
+
+use_git_config(user.name = "bastianolea", user.email = "baolea@uc.cl")
+
+# token
+create_github_token()
+
+gitcreds::gitcreds_set()
+
+
+
+# —----
+
+
